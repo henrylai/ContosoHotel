dde5b327b4f91b30b0fc4878f30e3a11f4c5c069
Henry Lai <bzlai@aol.com>
2017-03-06 19:20:24 -0800

    Added confirmation page to reservations

--- ContosoHotel/Controllers/RoomController.cs
+++ ContosoHotel/Controllers/RoomController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using ContosoHotel.DAL;
 using ContosoHotel.Models;
+using System.Diagnostics;
 
 namespace ContosoHotel.Controllers
 {
@@ -41,18 +42,36 @@ namespace ContosoHotel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Reserve([Bind(Include = "RoomID,RoomType,Vacancy,Price")] Room room)
+        public ActionResult Reserve([Bind(Include = "RoomID,RoomType,Vacancy,Price")] Room room, string firstName, string lastName, string email, string address, string phone)
         {
+            Debug.WriteLine(firstName + lastName + email + address + phone);
+            
             if (ModelState.IsValid)
             {
+                
                 db.Entry(room).State = EntityState.Modified;
-                room.Vacancy = false;
+                //room.Vacancy = false;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Confirm", new { roomID = room.RoomID, roomType = room.RoomType, roomPrice = room.Price,
+                    guestEmail = email, guestPhone = phone, guestName = firstName + " " + lastName, guestAddress = address });
             }
             return View(room);
         }
 
+
+        // GET: Confirm
+        public ActionResult Confirm(string roomID, string roomType, string roomPrice, string guestEmail, 
+                                    string guestPhone, string guestName, string guestAddress)
+        {
+            ViewData["roomID"] = roomID;
+            ViewData["roomType"] = roomType;
+            ViewData["roomPrice"] = roomPrice;
+            ViewData["guestEmail"] = guestEmail;
+            ViewData["guestPhone"] = guestPhone;
+            ViewData["guestName"] = guestName;
+            ViewData["guestAddress"] = guestAddress;
+            return View();
+        }
 
 
         // GET: Room
